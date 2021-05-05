a55ed247abd7e5e8ea65013e200acd25f4669cbe
Nicholas Kindrachuk <43053345+nkind@users.noreply.github.com>
2021-05-05 08:02:35 -0600

    Card layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,13 @@
 import React from "react";
 import { Helmet } from "react-helmet";
 import { makeStyles } from "@material-ui/core/styles";
-
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+// import CardMedia from "@material-ui/core/CardMedia";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
 import alphabet from "../images/alphabet.svg";
 import OrderForm from "../components/orderForm";
 
@@ -18,9 +24,25 @@ export default function Index() {
           href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
         />
       </Helmet>
-      <OrderForm />
-
       <div className={classes.backImage} />
+
+      <Card
+        className={classes.cardRoot}
+        style={{ backgroundColor: "white", opacity: 1 }}
+      >
+        <CardActionArea>
+          <CardContent>
+            <Typography>What is My Writing Dictionary?</Typography>
+            <Typography>It's a book to make lil guys write good.</Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <Button size="small" color="primary">
+            Sample
+          </Button>
+        </CardActions>
+      </Card>
+      <OrderForm />
     </div>
   );
 }
@@ -28,6 +50,12 @@ export default function Index() {
 const useStyles = makeStyles(() => ({
   root: {
     display: "flex",
+    flexShrink: 0,
+    justifyContent: "center",
+  },
+  cardRoot: {
+    marginTop: "20%",
+    zIndex: 1,
   },
   backImage: {
     backgroundImage: `url(${alphabet})`,
@@ -37,5 +65,6 @@ const useStyles = makeStyles(() => ({
     height: "100vh",
     width: "100vw",
     position: "fixed",
+    zIndex: 0,
   },
 }));
